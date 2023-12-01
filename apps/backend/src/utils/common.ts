7b0492112b758d35a727f0181a4a0fdea620e84c
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-01 22:48:07 +0000

    Add __DEV__ util

--- apps/backend/src/utils/common.ts
+++ apps/backend/src/utils/common.ts
@@ -5,4 +5,6 @@ export const __filename = fileURLToPath(import.meta.url);
 
 export const __dirname = dirname(__filename);
 
+export const __DEV__ = process.env.NODE_ENV === "development";
+
 export const noop = () => {};
