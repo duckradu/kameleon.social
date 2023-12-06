92bc328b91d5b25725a534bcd766578c938d76dd
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-06 21:20:09 +0000

    Create Icon component

---
+++ apps/frontend/src/components/ui/icon.tsx
@@ -0,0 +1,120 @@
+import HeroiconsXMark20Solid from "~icons/heroicons/x-mark-20-solid";
+import LogoSolid from "~icons/local/logo-solid";
+import SolarAltArrowDownOutline from "~icons/solar/alt-arrow-down-outline";
+import SolarAltArrowLeftOutline from "~icons/solar/alt-arrow-left-outline";
+import SolarAltArrowRightOutline from "~icons/solar/alt-arrow-right-outline";
+import SolarBagHeartBold from "~icons/solar/bag-heart-bold";
+import SolarBagHeartOutline from "~icons/solar/bag-heart-outline";
+import SolarBellBold from "~icons/solar/bell-bold";
+import SolarBellOutline from "~icons/solar/bell-outline";
+import SolarBookmarkBold from "~icons/solar/bookmark-bold";
+import SolarBookmarkOutline from "~icons/solar/bookmark-outline";
+import SolarChatRoundBold from "~icons/solar/chat-round-bold";
+import SolarChatRoundOutline from "~icons/solar/chat-round-outline";
+import SolarCompassBold from "~icons/solar/compass-bold";
+import SolarCompassOutline from "~icons/solar/compass-outline";
+import SolarDangerTriangleBold from "~icons/solar/danger-triangle-bold";
+import SolarEyeBold from "~icons/solar/eye-bold";
+import SolarEyeOutline from "~icons/solar/eye-outline";
+import SolarFileTextBold from "~icons/solar/file-text-bold";
+import SolarFileTextOutline from "~icons/solar/file-text-outline";
+import SolarGalleryFavouriteBold from "~icons/solar/gallery-favourite-bold";
+import SolarGalleryFavouriteOutline from "~icons/solar/gallery-favourite-outline";
+import SolarHeartBold from "~icons/solar/heart-bold";
+import SolarHeartOutline from "~icons/solar/heart-outline";
+import SolarHomeSmileBold from "~icons/solar/home-smile-bold";
+import SolarHomeSmileOutline from "~icons/solar/home-smile-outline";
+import SolarLoginBold from "~icons/solar/login-bold";
+import SolarLoginOutline from "~icons/solar/login-outline";
+import SolarLogoutOutline from "~icons/solar/logout-outline";
+import SolarMenuDotsBold from "~icons/solar/menu-dots-bold";
+import SolarMenuDotsOutline from "~icons/solar/menu-dots-outline";
+import SolarRepeatOutline from "~icons/solar/repeat-outline";
+import SolarSettingsBold from "~icons/solar/settings-bold";
+import SolarSettingsOutline from "~icons/solar/settings-outline";
+import SolarTrafficLineDuotone from "~icons/solar/traffic-line-duotone";
+import SolarUserBold from "~icons/solar/user-bold";
+import SolarUserOutline from "~icons/solar/user-outline";
+import SolarUserPlusBold from "~icons/solar/user-plus-bold";
+import SolarUserPlusOutline from "~icons/solar/user-plus-outline";
+
+export const Icon = {
+  logo: {
+    solid: LogoSolid,
+  },
+
+  x: HeroiconsXMark20Solid,
+  menuDots: {
+    outline: SolarMenuDotsOutline,
+    solid: SolarMenuDotsBold,
+  },
+  spinner: SolarTrafficLineDuotone,
+  validationError: SolarDangerTriangleBold,
+  chevron: {
+    right: SolarAltArrowRightOutline,
+    bottom: SolarAltArrowDownOutline,
+    left: SolarAltArrowLeftOutline,
+  },
+
+  logout: SolarLogoutOutline,
+
+  home: {
+    outline: SolarHomeSmileOutline,
+    solid: SolarHomeSmileBold,
+  },
+  news: {
+    outline: SolarFileTextOutline,
+    solid: SolarFileTextBold,
+  },
+  compass: {
+    outline: SolarCompassOutline,
+    solid: SolarCompassBold,
+  },
+  shop: {
+    outline: SolarBagHeartOutline,
+    solid: SolarBagHeartBold,
+  },
+  userPlus: {
+    outline: SolarUserPlusOutline,
+    solid: SolarUserPlusBold,
+  },
+  login: {
+    outline: SolarLoginOutline,
+    solid: SolarLoginBold,
+  },
+  user: {
+    outline: SolarUserOutline,
+    solid: SolarUserBold,
+  },
+  bell: {
+    outline: SolarBellOutline,
+    solid: SolarBellBold,
+  },
+  galleryFavourite: {
+    outline: SolarGalleryFavouriteOutline,
+    solid: SolarGalleryFavouriteBold,
+  },
+  gear: {
+    outline: SolarSettingsOutline,
+    solid: SolarSettingsBold,
+  },
+  heart: {
+    outline: SolarHeartOutline,
+    solid: SolarHeartBold,
+  },
+  eye: {
+    outline: SolarEyeOutline,
+    solid: SolarEyeBold,
+  },
+  repeat: SolarRepeatOutline,
+  chat: {
+    round: {
+      outline: SolarChatRoundOutline,
+      solid: SolarChatRoundBold,
+    },
+  },
+  bookmark: {
+    outline: SolarBookmarkOutline,
+    solid: SolarBookmarkBold,
+  },
+};
