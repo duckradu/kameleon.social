d6624ff802d3ae92f2154d6f5278262ecf502bb6
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-06 21:19:35 +0000

    Add css variables & font

--- apps/frontend/uno.config.ts
+++ apps/frontend/uno.config.ts
@@ -7,7 +7,64 @@ import {
 } from "unocss";
 
 export default defineConfig({
-  theme: {},
-  presets: [presetUno(), presetWebFonts()],
+  theme: {
+    colors: {
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
+
+      popover: {
+        DEFAULT: "hsl(var(--popover))",
+        foreground: "hsl(var(--popover-foreground))",
+      },
+
+      brand: {
+        DEFAULT: "hsl(var(--brand))",
+        foreground: "hsl(var(--brand-foreground))",
+      },
+
+      primary: {
+        DEFAULT: "hsl(var(--primary))",
+        foreground: "hsl(var(--primary-foreground))",
+      },
+
+      secondary: {
+        DEFAULT: "hsl(var(--secondary))",
+        foreground: "hsl(var(--secondary-foreground))",
+      },
+
+      muted: {
+        DEFAULT: "hsl(var(--muted))",
+        foreground: "hsl(var(--muted-foreground))",
+      },
+
+      accent: {
+        DEFAULT: "hsl(var(--accent))",
+        foreground: "hsl(var(--accent-foreground))",
+      },
+
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+    },
+    borderRadius: {
+      sm: "calc(var(--radius) - 4px)",
+      md: "calc(var(--radius) - 2px)",
+      lg: "var(--radius)",
+      xl: "calc(var(--radius) + 2px)",
+      "2xl": "calc(var(--radius) + 4px)",
+    },
+  },
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "bunny",
+      fonts: {
+        sans: {
+          name: "Noto Sans",
+          weights: ["400", "500", "600", "700"],
+        },
+      },
+    }),
+  ],
   transformers: [transformerVariantGroup(), transformerDirectives()],
 });
