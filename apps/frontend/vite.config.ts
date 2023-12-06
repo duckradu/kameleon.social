4e290254010ec50147ce81a7fa04722639fd0e7e
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-06 21:19:19 +0000

    Config stuff for icons

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import solid from "solid-start/vite";
 import UnoCSS from "unocss/vite";
+import Icons from "unplugin-icons/vite";
+import { FileSystemIconLoader } from "unplugin-icons/loaders";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [UnoCSS(), solid()],
+  plugins: [
+    Icons({
+      customCollections: {
+        local: FileSystemIconLoader("./public/assets/icons"),
+      },
+      compiler: "solid",
+    }),
+    UnoCSS(),
+    solid(),
+  ],
 });
