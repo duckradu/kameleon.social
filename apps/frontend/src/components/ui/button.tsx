021bf90c8318c1d7df6e52eba10af00377b2e0d4
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-08 18:45:52 +0000

    Add Input & Button components

---
+++ apps/frontend/src/components/ui/button.tsx
@@ -0,0 +1,45 @@
+import { createMemo, JSX } from "solid-js";
+import { tv, type VariantProps } from "tailwind-variants";
+
+export const buttonVariants = tv({
+  base: [
+    "inline-flex justify-center items-center",
+    "rounded-md",
+    "text-sm font-medium",
+    "px-4",
+    "transition-colors",
+    "focus-visible-(outline-none ring-2 ring-ring ring-offset-2 ring-offset-background)",
+    "disabled-(opacity-50 pointer-events-none)",
+  ],
+  variants: {
+    variant: {
+      primary: "bg-primary text-primary-foreground hover:bg-primary/90",
+      secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/90",
+    },
+    dimension: {
+      sm: "h-10",
+      md: "h-12",
+      lg: "h-14 rounded-lg",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    dimension: "md",
+  },
+});
+
+export interface ButtonProps
+  extends JSX.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+export function Button(props: ButtonProps) {
+  const classes = createMemo(() =>
+    buttonVariants({
+      variant: props.variant,
+      dimension: props.dimension,
+      class: props.class,
+    })
+  );
+
+  return <button {...props} class={classes()} />;
+}
