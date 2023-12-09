524190a8d51c724b96cadd7775df7801b192b8b0
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-09 01:43:05 +0000

    PAIN

--- apps/frontend/src/lib/xfetch.ts
+++ apps/frontend/src/lib/xfetch.ts
@@ -1,4 +1,8 @@
 export async function xfetch(url: string, config: Parameters<typeof fetch>[1]) {
+  // TODO: Remember to remove
+  console.log(">>", url, /^https?:\/\//i.test(url), process?.env?.API_URL);
+  console.log(">>", import.meta.env.VITE_API_URL);
+
   const absoluteUrl = /^https?:\/\//i.test(url)
     ? url
     : `${process.env.API_URL || import.meta.env.VITE_API_URL}${url}`;
