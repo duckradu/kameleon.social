552a52f96e38e27276f66d44949070562ccb5d0a
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-09 00:40:09 +0000

    Add code to test prod

--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,3 +1,32 @@
+import { createRouteData, refetchRouteData, useRouteData } from "solid-start";
+
+import { Button } from "~/components/ui/button";
+import { Icon } from "~/components/ui/icon";
+
+export function routeData() {
+  return createRouteData(async () => {
+    const response = await fetch("http://127.0.0.1:3100/api/v1/test/", {
+      method: "GET",
+      headers: new Headers({ "content-type": "application/json" }),
+    });
+    const data = await response.json();
+
+    console.error(">>>", data);
+
+    return data as { hello: "world" };
+  });
+}
+
 export default function Home() {
-  return <main>HELLO WORLD</main>;
+  const data = useRouteData<typeof routeData>();
+
+  return (
+    <main class="flex items-center justify-center h-screen flex-col gap-8">
+      <Icon.logo.solid class="w-10 h-10 text-foreground" />
+
+      <span>Hello {data()?.hello}</span>
+
+      <Button onClick={() => refetchRouteData()}>Refetch</Button>
+    </main>
+  );
 }
