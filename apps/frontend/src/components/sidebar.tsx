c0547f58924891ecf75f119f462c1a1b505f3b64
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-15 23:22:25 +0000

    Add logo font. Adjust space/size

--- apps/frontend/src/components/sidebar.tsx
+++ apps/frontend/src/components/sidebar.tsx
@@ -9,13 +9,10 @@ import { Icon } from "~/components/ui/icon";
 export function Sidebar() {
   return (
     <div class="flex flex-col h-screen py-3">
-      <A href="/" class="flex items-center gap-2 px-2.5 pt-1 pb-2">
+      <A href="/" class="flex items-center gap-3 px-3 pt-1 pb-2">
         <Icon.logo.solid class="shrink-0 w-7 h-7" />
 
-        <div>
-          <span class="text-xl font-bold">kameleon</span>
-          <small class="text-muted-foreground">.social</small>
-        </div>
+        <span class="text-3xl font-logo">kameleon</span>
       </A>
 
       <div class="flex flex-col flex-grow justify-between gap-2">
