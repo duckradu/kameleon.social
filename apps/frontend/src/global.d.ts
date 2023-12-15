08a6c9cb82b71995581b7144b39092a4ac2d53a5
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-15 23:17:41 +0000

    Add SVGIcon type

--- apps/frontend/src/global.d.ts
+++ apps/frontend/src/global.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="solid-start/env" />
+
+import { JSX } from "solid-js";
+
+declare global {
+  type SVGIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => JSX.Element;
+}
+
+export {};
