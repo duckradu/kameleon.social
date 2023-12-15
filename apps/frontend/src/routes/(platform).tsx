f56764b39dc8a9538ee98952bdcc482f678fcc2c
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-15 23:18:50 +0000

    Adjust layout styles. Move border to the <main /> element

--- apps/frontend/src/routes/(platform).tsx
+++ apps/frontend/src/routes/(platform).tsx
@@ -5,15 +5,15 @@ import { Sidebar } from "~/components/sidebar";
 
 export default function PlatformLayout() {
   return (
-    <div class="flex [&>:first-child]:ml-auto [&>:last-child]:mr-auto [&>*]:px-3">
+    <div class="flex [&>:first-child]:ml-auto [&>:last-child]:mr-auto [&>aside]:px-3 [&>div>main]:px-3">
       <aside class="relative w-64 shrink-0 z-20">
         <div class="sticky top-0 w-full h-screen">
           <Sidebar />
         </div>
       </aside>
 
-      <div class="w-xl border-x">
-        <main class="relative w-full min-h-screen mx-auto">
+      <div class="w-full">
+        <main class="relative w-full max-w-xl min-h-screen mx-auto border-x">
           <Outlet />
         </main>
       </div>
