acda33ad67823127911aba3039b3b6446914b236
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-16 23:54:39 +0000

    Fix migrator not able to find migrations dir

--- apps/backend/src/db/migrate.ts
+++ apps/backend/src/db/migrate.ts
@@ -1,13 +1,17 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { dirname, join } from "path";
 import postgres from "postgres";
+import { fileURLToPath } from "url";
 
 import dbConfig from "~/config/db";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const pgClient = postgres(dbConfig.dbCredentials.connectionString, { max: 1 });
 
 const db = drizzle(pgClient);
 
-await migrate(db, { migrationsFolder: "drizzle" });
+await migrate(db, { migrationsFolder: join(__dirname, "migrations") });
 
 await pgClient.end();
