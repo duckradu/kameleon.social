4191cf2ba65f4855f241774fcc89753fa2bede59
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-16 16:03:35 +0000

    Generate new voyage-client

--- apps/backend/generated/voyage-client.ts
+++ apps/backend/generated/voyage-client.ts
@@ -2,32 +2,38 @@
 // ! Any changes will be overwritten next time this is generated.
 // ! To update the generated output look inside `/src/plugins/voyage-client-generator/index.ts`
 
-export interface SignInWithCredentials {
-  email: string;
-  password: string;
-}
-export interface SelectActor {
-  publicId: string;
-  email: string;
-  name: string | null;
-  handle: string;
-  bio: string | null;
-  dob: string | null;
-  locale: string;
-  coverUrl: string | null;
-  avatarUrl: string | null;
-  externalUrl: string | null;
-  emailVerifiedAt: string | null;
-}
+import { Static, Type } from "@sinclair/typebox";
+
+export type SignInWithCredentials = Static<typeof SignInWithCredentials>;
+export const SignInWithCredentials = Type.Object({
+  email: Type.String({ format: "email" }),
+  password: Type.String(),
+});
+
+export type SelectActor = Static<typeof SelectActor>;
+export const SelectActor = Type.Object({
+  publicId: Type.String({ minLength: 12, maxLength: 12 }),
+  email: Type.String(),
+  name: Type.Union([Type.String({ maxLength: 70 }), Type.Null()]),
+  handle: Type.String({ maxLength: 32 }),
+  bio: Type.Union([Type.String({ maxLength: 255 }), Type.Null()]),
+  dob: Type.Union([Type.String(), Type.Null()]),
+  locale: Type.String({ maxLength: 12 }),
+  coverUrl: Type.Union([Type.String(), Type.Null()]),
+  avatarUrl: Type.Union([Type.String(), Type.Null()]),
+  externalUrl: Type.Union([Type.String(), Type.Null()]),
+  emailVerifiedAt: Type.Union([Type.String(), Type.Null()]),
+});
 export interface auth_GET {
   200: SelectActor;
 }
-export interface AccessToken {
-  accessToken: string;
-}
+
+export type AccessToken = Static<typeof AccessToken>;
+export const AccessToken = Type.Object({ accessToken: Type.String() });
 export interface auth_refresh_POST {
   200: AccessToken;
 }
+
 export interface auth_POST {
   200: AccessToken;
 }
@@ -41,14 +47,19 @@ export const createVoyageClient = (
     method: string,
   ) => (payload: TPayload) => Promise<TReturnMap[keyof TReturnMap]>,
 ) => ({
+  test: { GET: Object.assign(callback("/api/v1/test", "GET")) },
   auth: {
-    PATCH: callback("/api/v1/auth", "PATCH"),
-    DELETE: callback("/api/v1/auth", "DELETE"),
-    GET: callback<void, auth_GET>("/api/v1/auth", "GET"),
+    PATCH: Object.assign(callback("/api/v1/auth", "PATCH")),
+    DELETE: Object.assign(callback("/api/v1/auth", "DELETE")),
+    GET: Object.assign(callback<void, auth_GET>("/api/v1/auth", "GET")),
     refresh: {
-      POST: callback<void, auth_refresh_POST>("/api/v1/auth/refresh", "POST"),
+      POST: Object.assign(
+        callback<void, auth_refresh_POST>("/api/v1/auth/refresh", "POST"),
+      ),
     },
-    POST: callback<SignInWithCredentials, auth_POST>("/api/v1/auth", "POST"),
+    POST: Object.assign(
+      callback<SignInWithCredentials, auth_POST>("/api/v1/auth", "POST"),
+      { $s: { body: SignInWithCredentials } },
+    ),
   },
-  test: { GET: callback("/api/v1/test", "GET") },
 });
