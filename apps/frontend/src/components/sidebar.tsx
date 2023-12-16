c1afc21d0946cad2ecb539fae8fe25e2c068d312
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-16 16:45:22 +0000

    Change logo font :D

--- apps/frontend/src/components/sidebar.tsx
+++ apps/frontend/src/components/sidebar.tsx
@@ -12,7 +12,7 @@ export function Sidebar() {
       <A href="/" class="flex items-center gap-3 px-3 pt-1 pb-2">
         <Icon.logo.solid class="shrink-0 w-7 h-7" />
 
-        <span class="text-3xl font-logo">kameleon</span>
+        <span class="text-3xl font-logo -mb-3">kameleon</span>
       </A>
 
       <div class="flex flex-col flex-grow justify-between gap-2">
