4f020faa4dd3e800892078e88882a71acc4c1b6d
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-16 00:47:24 +0000

    Yeah

--- apps/frontend/src/routes/(auth)/sign-in.tsx
+++ apps/frontend/src/routes/(auth)/sign-in.tsx
@@ -1,45 +1,33 @@
 import { A } from "solid-start";
 
-import { Button } from "~/components/ui/button";
+import { SignInForm } from "~/components/form/sign-in";
 import { Icon } from "~/components/ui/icon";
-import { Input } from "~/components/ui/input";
 
 export default function SignInPage() {
   return (
-    <div class="flex justify-center items-center p-4 h-screen">
-      <div class="space-y-6 max-w-sm w-full">
-        <div class="flex flex-col gap-2 text-center">
-          <A href="/" class="mx-auto">
-            <Icon.logo.solid class="w-7.5 h-7.5" />
-          </A>
-          <h1 class="text-2xl font-semibold tracking-tight">Welcome back</h1>
-          <p class="text-sm text-muted-foreground">
-            Enter your credentials to sign in to your account
-          </p>
-        </div>
+    <div class="space-y-6 max-w-sm w-full">
+      <div class="flex flex-col gap-2 text-center">
+        <A href="/" class="mx-auto">
+          <Icon.logo.solid class="w-7.5 h-7.5" />
+        </A>
+        <h1 class="text-2xl font-semibold tracking-tight">Welcome back</h1>
+        <p class="text-sm text-muted-foreground">
+          Enter your credentials to sign in to your account
+        </p>
+      </div>
 
-        <form class="grid gap-2">
-          <Input dimension="lg" type="email" placeholder="Email" />
-          <Input dimension="lg" type="password" placeholder="Password" />
-          <Button dimension="lg" type="submit">
-            Sign In
-          </Button>
-        </form>
+      <SignInForm />
 
-        <div class="flex justify-between text-sm text-muted-foreground">
-          <A
-            href="/sign-up"
-            class="underline underline-offset-4 hover:text-brand"
-          >
-            Create an account
-          </A>
-          <A
-            href="/recover-access"
-            class="underline underline-offset-4 hover:text-brand text-right"
-          >
-            Forgot your password?
-          </A>
-        </div>
+      <div class="flex justify-between text-sm text-muted-foreground">
+        <A href="/sign-up" class="hover:underline underline-offset-4">
+          Create an account
+        </A>
+        <A
+          href="/recover-access"
+          class="hover:underline underline-offset-4 text-right"
+        >
+          Forgot your password?
+        </A>
       </div>
     </div>
   );
