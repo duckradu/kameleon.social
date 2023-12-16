31bef124739bdac05564c71215828ed5d2f16ad9
2blackholes2 <radu.dascalu97@gmail.com>
2023-12-16 00:47:09 +0000

    Remove hover color

--- apps/frontend/src/components/footer.tsx
+++ apps/frontend/src/components/footer.tsx
@@ -5,19 +5,19 @@ export function Footer() {
   return (
     <div class="py-3 text-xs text-muted-foreground space-y-2">
       <nav class="flex flex-wrap gap-1 p-3 bg-muted rounded-md">
-        <A href="/" class="hover:underline hover:text-brand underline-offset-3">
+        <A href="/" class="hover:underline underline-offset-3">
           About us
         </A>
         &middot;
-        <A href="/" class="hover:underline hover:text-brand underline-offset-3">
+        <A href="/" class="hover:underline underline-offset-3">
           Terms & Privacy
         </A>
         &middot;
-        <A href="/" class="hover:underline hover:text-brand underline-offset-3">
+        <A href="/" class="hover:underline underline-offset-3">
           Help
         </A>
         &middot;
-        <A href="/" class="hover:underline hover:text-brand underline-offset-3">
+        <A href="/" class="hover:underline underline-offset-3">
           Send feedback
         </A>
       </nav>
