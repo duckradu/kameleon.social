1d7218b9af7c9173d949ccda0ae0b0b47a7d5083
2blackholes2 <radu.dascalu97@gmail.com>
2024-02-18 09:29:12 +0000

    Add global type utils

--- apps/web-app/src/global.d.ts
+++ apps/web-app/src/global.d.ts
@@ -1,1 +1,24 @@
 /// <reference types="@solidjs/start/env" />
+
+import { JSX } from "solid-js";
+
+declare global {
+  type SVGIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => JSX.Element;
+
+  type UnionToIntersection<U> = (
+    U extends any ? (k: U) => void : never
+  ) extends (k: infer I) => void
+    ? I
+    : never;
+
+  type VariantSlotsClassType<T extends any> = T extends string
+    ? {
+        [key in T as `${key}Class`]?: string;
+      }
+    : never;
+
+  type VariantSlotsClassProps<Component extends (...args: any) => any> =
+    UnionToIntersection<VariantSlotsClassType<keyof ReturnType<Component>>>;
+}
+
+export {};
