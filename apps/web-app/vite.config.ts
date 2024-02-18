0f184c19ea12bec82162af89829190d80f345623
2blackholes2 <radu.dascalu97@gmail.com>
2024-02-18 09:35:23 +0000

    Install & setup icons package

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from "unocss/vite";
+import Icons from "unplugin-icons/vite";
+import { FileSystemIconLoader } from "unplugin-icons/loaders";
 
 export default defineConfig({
-  plugins: [UnoCSS()],
+  plugins: [
+    Icons({
+      customCollections: {
+        local: FileSystemIconLoader("./public/icons"),
+      },
+      compiler: "solid",
+    }),
+    UnoCSS(),
+  ],
 });
