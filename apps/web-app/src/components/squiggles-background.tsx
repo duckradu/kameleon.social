e5c9462c8e4746e23b29c64203ac8eb59a16b646
2blackholes2 <radu.dascalu97@gmail.com>
2024-02-18 11:20:04 +0000

    Create squiggles bg component

---
+++ apps/web-app/src/components/squiggles-background.tsx
@@ -0,0 +1,18 @@
+export function SquigglesBackground() {
+  return (
+    <>
+      <img
+        src="/squiggles/squiggle-1.svg"
+        class="fixed -top-2 -left-2 w-[calc(20vh)] -z-1"
+      />
+      <img
+        src="/squiggles/squiggle-2.svg"
+        class="fixed top-1/8 -right-2 w-[6.5vh] -z-1"
+      />
+      <img
+        src="/squiggles/squiggle-3.svg"
+        class="fixed top-1/2 -left-2 w-[13vh] -z-1"
+      />
+    </>
+  );
+}
