b77878f0577249d1e4f96ba48e1695d0cdebbaa4
2blackholes2 <radu.dascalu97@gmail.com>
2024-02-18 10:40:39 +0000

    Testing trpc ssr

--- apps/web-app/src/routes/index.tsx
+++ apps/web-app/src/routes/index.tsx
@@ -1,7 +1,14 @@
+import { createAsync } from "@solidjs/router";
+
+import { api } from "~/lib/api";
+
 export default function HomePage() {
+  const rs = createAsync(() => api.example.hello.query());
+
   return (
     <div>
       <h1>Home</h1>
+      <pre>{JSON.stringify(rs(), null, 2)}</pre>
     </div>
   );
 }
