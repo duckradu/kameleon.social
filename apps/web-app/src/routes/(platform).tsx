0d5870b185a50ce1027af19e50ed0c86279cbfeb
2blackholes2 <radu.dascalu97@gmail.com>
2024-02-26 22:20:08 +0000

    Create platform layout

---
+++ apps/web-app/src/routes/(platform).tsx
@@ -0,0 +1,30 @@
+import { RouteSectionProps } from "@solidjs/router";
+
+import { Footer } from "~/components/footer";
+import { PlatformSidebar } from "~/components/platform-sidebar";
+
+export default function PlatformLayout(props: RouteSectionProps) {
+  return (
+    <div class="flex [&>:first-child]:ml-auto [&>:last-child]:mr-auto [&>aside]:px-2 [&>div>main]:px-2">
+      <aside class="relative w-64 shrink-0 z-20">
+        <div class="sticky top-0 w-full h-screen">
+          <PlatformSidebar />
+        </div>
+      </aside>
+
+      <div class="w-full">
+        <main class="relative w-full max-w-2xl min-h-screen mx-auto">
+          {props.children}
+        </main>
+      </div>
+
+      <aside class="relative w-74 shrink-0 z-10">
+        <div class="sticky top-0 w-full h-screen">
+          <div class="py-2">
+            <Footer />
+          </div>
+        </div>
+      </aside>
+    </div>
+  );
+}
