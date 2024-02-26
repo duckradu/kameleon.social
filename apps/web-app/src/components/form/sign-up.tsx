d058bb693193b40518833361ad43d81b725861bb
2blackholes2 <radu.dascalu97@gmail.com>
2024-02-26 19:30:10 +0000

    Add /sign-up route

---
+++ apps/web-app/src/components/form/sign-up.tsx
@@ -0,0 +1,48 @@
+import { useSubmission } from "@solidjs/router";
+import { Show } from "solid-js";
+
+import { Alert } from "~/components/ui/alert";
+import { Button } from "~/components/ui/button";
+import { FormFieldError } from "~/components/ui/form-field-error";
+import { Icon } from "~/components/ui/icon";
+import { Input } from "~/components/ui/input";
+
+import { signUp } from "~/server/modules/auth/actions";
+
+export function SignUpForm() {
+  const submission = useSubmission(signUp);
+
+  return (
+    <form action={signUp} method="post" class="grid gap-2">
+      <Show when={submission.result?.error}>
+        <Alert>{submission.result?.error}</Alert>
+      </Show>
+      <div class="grid gap-1">
+        <Input size="lg" name="email" type="email" placeholder="Email" />
+        <Show when={submission.result?.errors?.email}>
+          <FormFieldError>{submission.result!.errors!.email}</FormFieldError>
+        </Show>
+      </div>
+
+      <div class="grid gap-1">
+        <Input
+          size="lg"
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+        <Show when={submission.result?.errors?.password}>
+          <FormFieldError>{submission.result!.errors!.password}</FormFieldError>
+        </Show>
+      </div>
+
+      <Button size="lg" type="submit" disabled={submission.pending}>
+        {submission.pending ? (
+          <Icon.spinner class="w-6 h-6 animate-spin" />
+        ) : (
+          "Sign up"
+        )}
+      </Button>
+    </form>
+  );
+}
