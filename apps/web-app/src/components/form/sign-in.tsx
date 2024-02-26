1e864c80901c41272253b59111d1bca86cd3abac
2blackholes2 <radu.dascalu97@gmail.com>
2024-02-26 22:20:37 +0000

    You can now sign in :P

--- apps/web-app/src/components/form/sign-in.tsx
+++ apps/web-app/src/components/form/sign-in.tsx
@@ -1,24 +1,49 @@
-import { action, redirect, useSubmission } from "@solidjs/router";
+import { useSubmission } from "@solidjs/router";
+import { Show } from "solid-js";
 
+import { Alert } from "~/components/ui/alert";
 import { Button } from "~/components/ui/button";
+import { FormFieldError } from "~/components/ui/form-field-error";
 import { Icon } from "~/components/ui/icon";
 import { Input } from "~/components/ui/input";
-import { FormFieldError } from "~/components/ui/form-field-error";
+
+import { signIn } from "~/server/modules/auth/actions";
 
 export function SignInForm() {
+  const submission = useSubmission(signIn);
+
   return (
-    <form action={undefined} method="post" class="grid gap-2">
+    <form action={signIn} method="post" class="grid gap-2">
+      <Show when={submission.result?.error}>
+        <Alert>{submission.result?.error}</Alert>
+      </Show>
+
       <div class="grid gap-1">
         <Input size="lg" name="email" type="email" placeholder="Email" />
+        <Show when={submission.result?.errors?.email}>
+          <FormFieldError>{submission.result!.errors!.email}</FormFieldError>
+        </Show>
       </div>
-      <Input size="lg" name="password" type="password" placeholder="Password" />
-      {/* <Button size="lg" type="submit" disabled={formSubmission.pending}>
-        {formSubmission.pending ? (
+
+      <div class="grid gap-1">
+        <Input
+          size="lg"
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+        <Show when={submission.result?.errors?.password}>
+          <FormFieldError>{submission.result!.errors!.password}</FormFieldError>
+        </Show>
+      </div>
+
+      <Button size="lg" type="submit" disabled={submission.pending}>
+        {submission.pending ? (
           <Icon.spinner class="w-6 h-6 animate-spin" />
         ) : (
           "Sign in"
         )}
-      </Button> */}
+      </Button>
     </form>
   );
 }
