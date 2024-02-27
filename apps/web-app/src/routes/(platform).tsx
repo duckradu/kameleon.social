f6aa8f2fdae1f8b003d3ba413179fd21cf443ae4
2blackholes2 <radu.dascalu97@gmail.com>
2024-02-27 10:32:18 +0000

    Wrap platform layout with session provider

--- apps/web-app/src/routes/(platform).tsx
+++ apps/web-app/src/routes/(platform).tsx
@@ -1,30 +1,37 @@
 import { RouteSectionProps } from "@solidjs/router";
+import { getRequestEvent } from "solid-js/web";
+
+import { SessionProvider } from "~/components/context/session";
 
 import { Footer } from "~/components/footer";
 import { PlatformSidebar } from "~/components/platform-sidebar";
 
 export default function PlatformLayout(props: RouteSectionProps) {
+  const event = getRequestEvent();
+
   return (
-    <div class="flex [&>:first-child]:ml-auto [&>:last-child]:mr-auto [&>aside]:px-2 [&>div>main]:px-2">
-      <aside class="relative w-64 shrink-0 z-20">
-        <div class="sticky top-0 w-full h-screen">
-          <PlatformSidebar />
-        </div>
-      </aside>
-
-      <div class="w-full">
-        <main class="relative w-full max-w-2xl min-h-screen mx-auto">
-          {props.children}
-        </main>
-      </div>
-
-      <aside class="relative w-74 shrink-0 z-10">
-        <div class="sticky top-0 w-full h-screen">
-          <div class="py-2">
-            <Footer />
+    <SessionProvider sessionActor={event?.locals.sessionActor || null}>
+      <div class="flex [&>:first-child]:ml-auto [&>:last-child]:mr-auto [&>aside]:px-2 [&>div>main]:px-2">
+        <aside class="relative w-64 shrink-0 z-20">
+          <div class="sticky top-0 w-full h-screen">
+            <PlatformSidebar />
           </div>
+        </aside>
+
+        <div class="w-full">
+          <main class="relative w-full max-w-2xl min-h-screen mx-auto">
+            {props.children}
+          </main>
         </div>
-      </aside>
-    </div>
+
+        <aside class="relative w-74 shrink-0 z-10">
+          <div class="sticky top-0 w-full h-screen">
+            <div class="py-2">
+              <Footer />
+            </div>
+          </div>
+        </aside>
+      </div>
+    </SessionProvider>
   );
 }
