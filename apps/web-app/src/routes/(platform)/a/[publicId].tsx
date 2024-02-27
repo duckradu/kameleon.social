f307083a5df88f08787ead22bcba79f01c87dfe3
2blackholes2 <radu.dascalu97@gmail.com>
2024-02-27 19:07:33 +0000

    Add new routes just because

---
+++ apps/web-app/src/routes/(platform)/a/[publicId].tsx
@@ -0,0 +1,12 @@
+export default function Actor() {
+  return (
+    <div>
+      <div
+        class="bg-foreground/10 rounded-b-xl bg-cover bg-center"
+        style={{
+          height: "calc((100vw - 240px) / 5)",
+        }}
+      ></div>
+    </div>
+  );
+}
