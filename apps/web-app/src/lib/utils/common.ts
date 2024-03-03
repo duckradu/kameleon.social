bdcf72aa1c77fdb7ccd0cb5218d82db6a6349e39
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-03 20:15:05 +0000

    Get rid of await-to-js as the latest version of solid-start is complaining about it

--- apps/web-app/src/lib/utils/common.ts
+++ apps/web-app/src/lib/utils/common.ts
@@ -18,3 +18,20 @@ export const NANO_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz";
 export const NANO_LENGTH = 12;
 
 export const nanoid = customAlphabet(NANO_ALPHABET, NANO_LENGTH);
+
+// https://github.com/scopsy/await-to-js/blob/master/src/await-to-js.ts
+export function to<T, U = Error>(
+  promise: Promise<T>,
+  errorExt?: object
+): Promise<[U, undefined] | [null, T]> {
+  return promise
+    .then<[null, T]>((data: T) => [null, data])
+    .catch<[U, undefined]>((err: U) => {
+      if (errorExt) {
+        const parsedError = Object.assign({}, err, errorExt);
+        return [parsedError, undefined];
+      }
+
+      return [err, undefined];
+    });
+}
