a791b8889f5da6e0d58ca3323a44c195747ada35
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-03 20:01:12 +0000

    Update dependencies

---
+++ apps/web-app/app.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "@solidjs/start/config";
+import UnoCSS from "unocss/vite";
+import Icons from "unplugin-icons/vite";
+import { FileSystemIconLoader } from "unplugin-icons/loaders";
+
+export default defineConfig({
+  vite: {
+    plugins: [
+      Icons({
+        customCollections: {
+          local: FileSystemIconLoader("./public/icons"),
+        },
+        compiler: "solid",
+      }),
+      UnoCSS(),
+    ],
+  },
+  middleware: "./src/middleware.ts",
+});
