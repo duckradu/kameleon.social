ba470dc5979a5ad152e2deeb75de8e81c6d23e8b
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-03 20:22:13 +0000

    Small refactor

--- apps/web-app/src/server/modules/actors/rpc.ts
+++ apps/web-app/src/server/modules/actors/rpc.ts
@@ -6,14 +6,11 @@ import { eq } from "drizzle-orm";
 import { db } from "~/server/db";
 import { actors } from "~/server/db/schemas/actors";
 
-import { serverErrorResponse, serverSuccessResponse } from "~/lib/utils/server";
 import { to } from "~/lib/utils/common";
 
 import { paths } from "~/lib/constants/paths";
 
 export async function findOneByPID$(pid: string) {
-  "use server";
-
   const [err, matchingActor] = await to(
     db.select().from(actors).where(eq(actors.pid, pid))
   );
@@ -21,7 +18,6 @@ export async function findOneByPID$(pid: string) {
   if (err) {
     // make a 500 page
     throw redirect(paths.notFound);
-    // return serverErrorResponse(err);
   }
 
   if (!matchingActor[0]) {
