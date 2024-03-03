1f6cfc0ca0fe1bce94cfb8e53a8302e16d682e7f
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-03 20:41:12 +0000

    Remove relative from main content container. Sync z-index between sides

--- apps/web-app/src/routes/(platform).tsx
+++ apps/web-app/src/routes/(platform).tsx
@@ -10,16 +10,17 @@ export default function PlatformLayout(props: RouteSectionProps) {
   const event = getRequestEvent();
 
   return (
-    <SessionProvider sessionActor={event?.locals.sessionActor || null}>
+    <SessionProvider sessionActor={event?.locals.sessionActor}>
       <div class="flex [&>:first-child]:ml-auto [&>:last-child]:mr-auto [&>aside]:px-2 [&>div>main]:px-2">
-        <aside class="relative w-64 shrink-0 z-20">
+        <aside class="relative w-64 shrink-0 z-10">
           <div class="sticky top-0 w-full h-screen">
             <PlatformSidebar />
           </div>
         </aside>
 
-        <div class="w-full">
-          <main class="relative w-full max-w-2xl min-h-screen mx-auto">
+        {/* TODO: Replace to w-full */}
+        <div class="w-2xl">
+          <main class="w-full max-w-2xl min-h-screen mx-auto">
             {props.children}
           </main>
         </div>
