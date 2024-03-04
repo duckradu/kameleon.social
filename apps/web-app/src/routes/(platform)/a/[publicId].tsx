9bcaa0a24bd1fcb756fc1bd7ed8620c57f482ffa
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-04 11:05:33 +0000

    WIP actor profile header & navigation

--- apps/web-app/src/routes/(platform)/a/[publicId].tsx
+++ apps/web-app/src/routes/(platform)/a/[publicId].tsx
@@ -1,26 +1,103 @@
-import { RouteDefinition, createAsync, useParams } from "@solidjs/router";
+import {
+  A,
+  RouteDefinition,
+  RouteSectionProps,
+  createAsync,
+  useParams,
+} from "@solidjs/router";
+import { format } from "date-fns/format";
+import { Show } from "solid-js";
+
+import { Avatar } from "~/components/ui/avatar";
+import { Button } from "~/components/ui/button";
+import { Icon } from "~/components/ui/icon";
 
 import { findOneByPID } from "~/server/modules/actors/actions";
+
+import { paths } from "~/lib/constants/paths";
 
 export const route = {
   load: ({ params }) => findOneByPID(params.publicId),
 } satisfies RouteDefinition;
 
-export default function Actor() {
+export default function ActorLayout(props: RouteSectionProps) {
   const params = useParams();
 
   const actor = createAsync(() => findOneByPID(params.publicId));
 
   return (
-    <div>
-      <div
-        class="bg-foreground/10 rounded-b-xl bg-cover bg-center"
-        style={{
-          height: "calc((100vw - 240px) / 5)",
-        }}
-      ></div>
+    <div class="space-y-4">
+      {/* <div class="w-screen h-screen absolute w-full left-0 right-0 -z-1 bg-foreground/10" /> */}
 
-      <h1>{actor()?.name}</h1>
+      <header class="space-y-2">
+        <div>
+          <div class="h-[200px] bg-brand/50 rounded-b-xl" />
+
+          <div class="relative grid grid-cols-3">
+            <div />
+
+            <div class="flex flex-col items-center justify-center space-y-2">
+              <Avatar size="profile" fallback="RD" rootClass="-mt-12" />
+
+              <div class="text-center">
+                <h1 class="text-xl font-semibold">{actor()?.name}</h1>
+                <A
+                  href={paths.actor.profile(actor()?.pid || "")}
+                  class="text-muted-foreground text-sm hover:underline"
+                >
+                  @{actor()?.pid}
+                </A>
+              </div>
+            </div>
+
+            <div class="flex justify-end pt-2">
+              <Button size="sm">Follow</Button>
+            </div>
+          </div>
+        </div>
+
+        <p class="text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          efficitur orci non hendrerit consectetur.
+        </p>
+
+        <div class="flex py-1 justify-evenly text-sm [&>span>svg]-(inline-flex mr-1)">
+          <span>
+            <Icon.map.pin.outline />
+            London
+          </span>
+          <span>
+            <Icon.link.minimalistic.outline />
+            <A
+              href="https://kameleon.social"
+              class="text-brand hover:underline"
+            >
+              kameleon.social
+            </A>
+          </span>
+          <Show when={actor()?.createdAt}>
+            <span>
+              <Icon.calendar.outline />
+              Joined {format(new Date(actor()!.createdAt), "MMM yyyy")}
+            </span>
+          </Show>
+          <Show when={actor()?.createdAt}>
+            <span>
+              <Icon.cake.outline />
+              Cake day {format(new Date(actor()!.createdAt), "MMM yyyy")}
+            </span>
+          </Show>
+        </div>
+      </header>
+
+      <div class="flex [&>a]-(flex flex-1 items-center justify-center h-12 font-medium border-b border-brand) [&>a:not(.active)]-(text-muted-foreground border-muted)">
+        <A href={paths.actor.profile(actor()?.pid || "")} end>
+          Activity
+        </A>
+        <A href={paths.actor.connections(actor()?.pid || "")}>Connections</A>
+      </div>
+
+      {props.children}
     </div>
   );
 }
