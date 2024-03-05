51051b3ac25ffaaf3317764e4245e36e49af8fc0
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-05 13:31:12 +0000

    Create `invite-codes` schema and connect with `actors`

--- apps/web-app/src/server/db/schemas/actors.ts
+++ apps/web-app/src/server/db/schemas/actors.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
   boolean,
   index,
@@ -8,13 +9,17 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 
+import { inviteCodes } from "~/server/db/schemas/invite-codes";
+
 import { nanoid } from "~/lib/utils/common";
 
 export const actors = pgTable(
   "actors",
   {
     id: serial("id").primaryKey(),
-    pid: varchar("pid").notNull().unique().$defaultFn(nanoid), // Generate handles
+    pid: varchar("pid").notNull().unique().$defaultFn(nanoid), // TODO: Generate nicer handles
+
+    acceptedInviteCode: varchar("accepted_invite_code"),
 
     email: varchar("email").notNull().unique(),
     encryptedPassword: varchar("encrypted_password").notNull(),
@@ -37,6 +42,7 @@ export const actors = pgTable(
     emailConfirmedAt: timestamp("email_confirmed_at", { mode: "string" }),
     // passwordExpiresAt: timestamp("password_expires_at", { mode: "string" }),
 
+    // * Represents `used_at` for `accepted_invite_code`
     createdAt: timestamp("created_at", { mode: "string" })
       .notNull()
       .defaultNow(),
@@ -46,3 +52,11 @@ export const actors = pgTable(
     indexActorsOnPid: index("index_actors_on_pid").on(table.pid),
   })
 );
+
+export const actorsRelations = relations(actors, ({ one, many }) => ({
+  acceptedInvite: one(inviteCodes, {
+    fields: [actors.acceptedInviteCode],
+    references: [inviteCodes.code],
+  }),
+  inviteCodes: many(inviteCodes),
+}));
---
+++ apps/web-app/src/server/db/schemas/invite-codes.ts
@@ -0,0 +1,32 @@
+import { relations } from "drizzle-orm";
+import {
+  integer,
+  pgTable,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+import { actors } from "~/server/db/schemas/actors";
+
+import { nanoid } from "~/lib/utils/common";
+
+export const inviteCodes = pgTable("invitation_codes", {
+  code: varchar("code").primaryKey().$defaultFn(nanoid),
+
+  issuerId: serial("issuer_id").notNull(),
+
+  availableUses: integer("available_uses").notNull(),
+
+  expiresAt: timestamp("expires_at", { mode: "string" }).notNull(),
+
+  createdAt: timestamp("created_at", { mode: "string" }).notNull().defaultNow(),
+});
+
+export const inviteCodesRelations = relations(inviteCodes, ({ one, many }) => ({
+  issuer: one(actors, {
+    fields: [inviteCodes.issuerId],
+    references: [actors.id],
+  }),
+  usedByActorIds: many(actors),
+}));
