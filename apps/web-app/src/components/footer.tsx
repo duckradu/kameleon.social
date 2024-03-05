3a072e3c26a3dffd7ffe22dd8d9951d1d91c3716
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-05 13:02:33 +0000

    Yeah

--- apps/web-app/src/components/footer.tsx
+++ apps/web-app/src/components/footer.tsx
@@ -1,23 +1,38 @@
 import { A } from "@solidjs/router";
 import { format } from "date-fns/format";
 
+import { paths } from "~/lib/constants/paths";
+
 export function Footer() {
+  // TODO: Probably move the paths to constants. Or use .env for actor id
   return (
     <footer class="text-xs text-muted-foreground space-y-2">
       <nav class="flex flex-wrap gap-1 p-3 bg-muted rounded-md">
-        <A href="/" class="hover:underline underline-offset-3">
+        <A
+          href={paths.actor("first_kameleon").record("about-us")}
+          class="hover:underline underline-offset-3"
+        >
           About us
         </A>
         &middot;
-        <A href="/" class="hover:underline underline-offset-3">
+        <A
+          href={paths.actor("first_kameleon").record("terms-and-privacy")}
+          class="hover:underline underline-offset-3"
+        >
           Terms & Privacy
         </A>
         &middot;
-        <A href="/" class="hover:underline underline-offset-3">
+        <A
+          href={paths.actor("first_kameleon").record("help")}
+          class="hover:underline underline-offset-3"
+        >
           Help
         </A>
         &middot;
-        <A href="/" class="hover:underline underline-offset-3">
+        <A
+          href={paths.actor("first_kameleon").record("send-feedback")}
+          class="hover:underline underline-offset-3"
+        >
           Send feedback
         </A>
       </nav>
