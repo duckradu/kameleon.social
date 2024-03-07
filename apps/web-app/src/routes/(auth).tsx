6acb946e448f75b4764ddb7449cee56f3108c2f6
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-07 15:54:11 +0000

    Minus one TODO

--- apps/web-app/src/routes/(auth).tsx
+++ apps/web-app/src/routes/(auth).tsx
@@ -1,9 +1,15 @@
-import { RouteSectionProps } from "@solidjs/router";
+import { Navigate, RouteSectionProps } from "@solidjs/router";
 
 import { CentralFocusLayout } from "~/components/central-focus-layout";
+import { useSession } from "~/components/context/session";
 
-// TODO: Redirect away if already logged in
 export default function AuthLayout(props: RouteSectionProps) {
+  const { actor } = useSession();
+
+  if (actor()) {
+    return <Navigate href="/" />;
+  }
+
   return (
     <CentralFocusLayout>
       <main class="max-w-sm w-full space-y-6">{props.children}</main>
