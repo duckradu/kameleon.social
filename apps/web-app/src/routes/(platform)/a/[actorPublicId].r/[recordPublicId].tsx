4d75baffb069779dcbf5c195e02c8e5d852c1dea
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-13 18:52:51 +0000

    Add border on scroll

--- apps/web-app/src/routes/(platform)/a/[actorPublicId].r/[recordPublicId].tsx
+++ apps/web-app/src/routes/(platform)/a/[actorPublicId].r/[recordPublicId].tsx
@@ -1,12 +1,37 @@
+import { createEffect, createSignal, onCleanup } from "solid-js";
+
 import { Record as RecordComponent } from "~/components/record";
 import { RecordFeed } from "~/components/record-feed";
 import { Button } from "~/components/ui/button";
 import { Icon } from "~/components/ui/icon";
 
 export default function Record() {
+  const [noScrollY, setNoScrollY] = createSignal(true);
+
+  createEffect(() => {
+    const event = "scroll";
+    const callback = () => {
+      if (window.scrollY === 0 && noScrollY() === false) {
+        setNoScrollY(true);
+      }
+      if (window.scrollY !== 0 && noScrollY() === true) {
+        setNoScrollY(false);
+      }
+    };
+
+    window.addEventListener(event, callback);
+
+    onCleanup(() => window.removeEventListener(event, callback));
+  });
+
   return (
     <div class="relative no-layout-space">
-      <div class="sticky-header py-layout z-10">
+      <div
+        classList={{
+          "sticky-header py-layout z-10 border-b border-transparent": true,
+          "border-border": !noScrollY(),
+        }}
+      >
         <Button
           size="sm"
           variant="secondary"
