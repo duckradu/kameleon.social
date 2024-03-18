c0c2fa291ae70477b8442dbe8dd25fcff97d2ee8
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-18 08:45:34 +0000

    WIP Toast component

---
+++ apps/web-app/src/components/context/toast.tsx
@@ -0,0 +1,78 @@
+import { PropTypes, normalizeProps, useMachine } from "@zag-js/solid";
+import * as toast from "@zag-js/toast";
+import {
+  Accessor,
+  For,
+  JSX,
+  ParentProps,
+  createContext,
+  createMemo,
+  createUniqueId,
+  useContext,
+} from "solid-js";
+
+import { Toast } from "~/components/ui/toast";
+
+export interface ToastOptions {
+  title: JSX.Element;
+  description: JSX.Element;
+
+  render?: (api: Accessor<toast.Api<PropTypes, ToastOptions>>) => JSX.Element;
+}
+
+const ToastContext =
+  createContext<Accessor<toast.GroupApi<PropTypes, ToastOptions>>>();
+
+export function ToastProvider(props: ParentProps) {
+  const [state, send] = useMachine(
+    toast.group.machine<ToastOptions>({
+      id: createUniqueId(),
+
+      gutter: "0.75rem", // TODO: replace with theme.spacing.xs
+      offsets: {
+        top: "0.75rem",
+        right: "0.75rem",
+        bottom: "0.75rem",
+        left: "0.75rem",
+      },
+      placement: "bottom-end",
+
+      duration: 100000,
+    })
+  );
+
+  const api = createMemo(() =>
+    toast.group.connect(state, send, normalizeProps)
+  );
+
+  return (
+    <ToastContext.Provider value={api}>
+      <For each={Object.entries(api().toastsByPlacement)}>
+        {([placement, toasts]) => (
+          <div
+            style={{
+              gap: "var(--toast-gutter)",
+            }}
+            {...api().getGroupProps({
+              placement: placement as toast.Placement,
+            })}
+          >
+            <For each={toasts}>{(toast) => <Toast actor={toast} />}</For>
+          </div>
+        )}
+      </For>
+
+      {props.children}
+    </ToastContext.Provider>
+  );
+}
+
+export function useToast() {
+  const context = useContext(ToastContext);
+
+  if (context === undefined) {
+    throw new Error("`useToast` must be used within a ToastProvider");
+  }
+
+  return context;
+}
---
+++ apps/web-app/src/components/ui/toast.tsx
@@ -0,0 +1,81 @@
+import { normalizeProps, useActor } from "@zag-js/solid";
+import * as toast from "@zag-js/toast";
+import { createMemo } from "solid-js";
+import { tv } from "tailwind-variants";
+
+import { ToastOptions, useToast } from "~/components/context/toast";
+import { Button } from "~/components/ui/button";
+import { Icon } from "~/components/ui/icon";
+
+export const toastVariants = tv({
+  slots: {
+    root: [
+      "relative pointer-events-auto overflow-hidden group",
+      "border rounded-md",
+      "flex items-center justify-between gap-2",
+      "w-388px p-4 pr-6",
+    ],
+  },
+  variants: {
+    variant: {
+      default: {
+        root: "bg-background/70 backdrop-blur-xl text-foreground border-border hover:border-muted-foreground/50",
+      },
+      destructive: {
+        root: "bg-destructive/90 backdrop-blur-xl text-destructive-foreground border-destructive/20 hover:bg-destructive",
+      },
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
+
+export type ToastProps = {
+  actor: toast.Service<ToastOptions>;
+};
+
+export function Toast(props: ToastProps) {
+  const [state, send] = useActor(props.actor);
+
+  const api = createMemo(() => toast.connect(state, send, normalizeProps));
+  const classes = createMemo(() =>
+    toastVariants({
+      variant: api().type === "error" ? "destructive" : "default",
+    })
+  );
+
+  if (state.context.render) {
+    return state.context.render(api);
+  }
+
+  return (
+    <div class={classes().root()}>
+      <div class="grid gap-1">
+        <h3 {...api().titleProps} class="text-sm font-semibold">
+          {api().title}
+        </h3>
+        <p {...api().descriptionProps} class="text-xs opacity-90">
+          {api().description}
+        </p>
+      </div>
+      <Button
+        size="xs"
+        shape="squircle"
+        variant="ghost"
+        onClick={api().dismiss}
+        class="text-base rounded-sm absolute top-1 right-1 transition-opacity opacity-0 focus:opacity-100 group-hover:opacity-100"
+      >
+        <Icon.x />
+      </Button>
+    </div>
+  );
+}
+
+export function TriggerToast(props: Partial<toast.ToastOptions<ToastOptions>>) {
+  const toast = useToast();
+
+  toast().create(props);
+
+  return null;
+}
