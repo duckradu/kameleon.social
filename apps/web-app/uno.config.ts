04d280108b0e8f62cdd30e15dfe306db233e64e2
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-18 08:42:49 +0000

    WIP styles

--- apps/web-app/uno.config.ts
+++ apps/web-app/uno.config.ts
@@ -13,9 +13,23 @@ export default defineConfig({
       background: "hsl(var(--background))",
       foreground: "hsl(var(--foreground))",
 
+      muted: {
+        DEFAULT: "hsl(var(--muted))",
+        foreground: "hsl(var(--muted-foreground))",
+      },
+
       popover: {
         DEFAULT: "hsl(var(--popover))",
         foreground: "hsl(var(--popover-foreground))",
+      },
+
+      primary: {
+        DEFAULT: "hsl(var(--primary))",
+        foreground: "hsl(var(--primary-foreground))",
+      },
+      secondary: {
+        DEFAULT: "hsl(var(--secondary))",
+        foreground: "hsl(var(--secondary-foreground))",
       },
 
       brand: {
@@ -23,24 +37,14 @@ export default defineConfig({
         foreground: "hsl(var(--brand-foreground))",
       },
 
-      primary: {
-        DEFAULT: "hsl(var(--primary))",
-        foreground: "hsl(var(--primary-foreground))",
-      },
-
-      secondary: {
-        DEFAULT: "hsl(var(--secondary))",
-        foreground: "hsl(var(--secondary-foreground))",
-      },
-
-      muted: {
-        DEFAULT: "hsl(var(--muted))",
-        foreground: "hsl(var(--muted-foreground))",
-      },
-
       accent: {
         DEFAULT: "hsl(var(--accent))",
         foreground: "hsl(var(--accent-foreground))",
+      },
+
+      destructive: {
+        DEFAULT: "hsl(var(--destructive))",
+        foreground: "hsl(var(--destructive-foreground))",
       },
 
       border: "hsl(var(--border))",
