3324186ce2a11121022e63bb903c8e18e5bcbdde
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-20 08:54:54 +0000

    WIP Records schemas

--- apps/web-app/src/server/db/schemas/records.ts
+++ apps/web-app/src/server/db/schemas/records.ts
@@ -1,51 +1,69 @@
-import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  index,
+  integer,
+  jsonb,
+  pgTable,
+  primaryKey,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 import { nanoid } from "~/lib/utils/common";
 
-// WIP
 export const records = pgTable(
   "records",
   {
     id: serial("id").primaryKey(),
     pid: varchar("pid").notNull().$defaultFn(nanoid), // TODO: Get a nicer gen
 
-    authorId: serial("author_id").notNull(),
+    authorId: integer("author_id").notNull(),
 
-    // versions:
-    // latestVersionId:
+    latestVersionId: integer("latest_version_id").notNull(),
 
     createdAt: timestamp("created_at", { mode: "string" })
       .notNull()
       .defaultNow(),
+    updatedAt: timestamp("updated_at", { mode: "string" }),
   },
   (table) => ({
-    // indexOnAuthorAndPID
+    pkOnAuthorAndPid: primaryKey({ columns: [table.authorId, table.pid] }),
   })
 );
+
+export const recordsRelations = relations(records, ({ one, many }) => ({
+  versions: many(recordVersions),
+  latestVersion: one(recordVersions, {
+    fields: [records.latestVersionId],
+    references: [recordVersions.id],
+  }),
+}));
 
 export const recordVersions = pgTable("record_versions", {
   id: serial("id").primaryKey(),
 
-  authorId: serial("author_id").notNull(),
+  authorId: integer("author_id").notNull(),
+  recordId: integer("recordId").notNull(),
 
-  // postId
-
-  // title?: string
-  // desc?: string
-
-  // content: RecordContent
+  content: jsonb("content"),
 
   createdAt: timestamp("created_at", { mode: "string" }).notNull().defaultNow(),
 });
 
-// WB blocks?
+export const recordVersionsRelations = relations(recordVersions, ({ one }) => ({
+  record: one(records, {
+    fields: [recordVersions.recordId],
+    references: [records.id],
+  }),
+}));
 
-export const recordBlockJSON = pgTable("record_block_json", {});
+// export const recordBlockJSON = pgTable("record_block_json", {});
 
-export const recordBlockImages = pgTable("record_block_images", {});
+// export const recordBlockImages = pgTable("record_block_images", {});
 
-export const recordBlockGalleries = pgTable("record_block_galleries", {});
+// export const recordBlockGalleries = pgTable("record_block_galleries", {});
 
-export const recordBlockVideos = pgTable("record_block_videos", {});
+// export const recordBlockVideos = pgTable("record_block_videos", {});
 
-export const recordBlockAudios = pgTable("record_block_audios", {});
+// export const recordBlockAudios = pgTable("record_block_audios", {});
