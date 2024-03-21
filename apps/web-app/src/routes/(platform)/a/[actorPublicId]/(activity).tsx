4cdd718c160929359aec7fbe3ce5eca95faa794d
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-21 08:32:21 +0000

    Render actor records

--- apps/web-app/src/routes/(platform)/a/[actorPublicId]/(activity).tsx
+++ apps/web-app/src/routes/(platform)/a/[actorPublicId]/(activity).tsx
@@ -1,5 +1,24 @@
+import { RouteDefinition, createAsync, useParams } from "@solidjs/router";
+
 import { RecordFeed } from "~/components/record-feed";
 
+import { getRecordListByAuthorPid } from "~/server/modules/records/actions";
+
+export const route = {
+  load: ({ params }) => getRecordListByAuthorPid(params.actorPublicId),
+} satisfies RouteDefinition;
+
 export default function ActorActivity() {
-  return <RecordFeed recordList={new Array(10)} class="py-layout" />;
+  const params = useParams();
+
+  const records = createAsync(() =>
+    getRecordListByAuthorPid(params.actorPublicId)
+  );
+
+  return (
+    <RecordFeed
+      recordList={(records()?.data || []) as any[]}
+      class="py-layout"
+    />
+  );
 }
