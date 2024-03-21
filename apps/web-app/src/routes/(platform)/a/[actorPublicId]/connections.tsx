576cef0e2408a4553d8d25e915a147e756f62227
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-21 15:21:50 +0000

    Nice

--- apps/web-app/src/routes/(platform)/a/[actorPublicId]/connections.tsx
+++ apps/web-app/src/routes/(platform)/a/[actorPublicId]/connections.tsx
@@ -1,3 +1,43 @@
+import { Show } from "solid-js";
+
+import { EmptyProfilePageMessage } from "~/components/empty-profile-page-message";
+
+import { sample } from "~/lib/utils/common";
+
+// TODO: DIFFERENT MESSAGES FOR VISITOR AND SAME SESSION
+const NO_DATA_MESSAGES = {
+  title: [
+    "Feels a little empty here",
+    "Welcome to your network!",
+    "New to the community?",
+    "Welcome to the social scene!",
+  ],
+  description: [
+    "Let's start building meaningful connections...",
+    "Your connections count may be zero now, but every great journey starts somewhere!",
+    "Your connections list is ready for growth...",
+    "Your network is waiting to expand! Start connecting with like-minded individuals...",
+    "From 0 to a thriving network! Your connections list is ready for action...",
+    "Your connections are waiting to be made! Let's start building bridges...",
+    "Your connections list is your key to community...",
+    "Zero connections now, but soon your network will be buzzing with activity!",
+  ],
+};
+
 export default function ActorConnections() {
-  return <div>connections</div>;
+  return (
+    <Show
+      when={false}
+      fallback={
+        <EmptyProfilePageMessage
+          title={sample(NO_DATA_MESSAGES.title)}
+          description={sample(NO_DATA_MESSAGES.description)}
+        >
+          <div>Explore | Suggest who to follow</div>
+        </EmptyProfilePageMessage>
+      }
+    >
+      Connections list
+    </Show>
+  );
 }
--- apps/web-app/src/routes/(platform)/a/[actorPublicId]/moments.tsx
+++ apps/web-app/src/routes/(platform)/a/[actorPublicId]/moments.tsx
@@ -1,3 +1,44 @@
+import { Show } from "solid-js";
+
+import { EmptyProfilePageMessage } from "~/components/empty-profile-page-message";
+import { Button } from "~/components/ui/button";
+
+import { sample } from "~/lib/utils/common";
+
+const NO_DATA_MESSAGES = {
+  title: [
+    "Lights, camera, action!",
+    "New to the scene?",
+    "Welcome to your moments!",
+    "Capture life's magic!",
+    "Welcome to your digital space!",
+  ],
+  description: [
+    "Your moments section is waiting...",
+    "Time to create your first masterpiece...",
+    "Your moments section is your canvas for creativity...",
+    "Let your story unfold...",
+    "Welcome to the spotlight! This is your stage...",
+    "Time to start collecting memories...",
+    "Your moments tab is empty for now...",
+    "Let's fill it with moments...",
+  ],
+};
+
 export default function ActorMoments() {
-  return <div>mm</div>;
+  return (
+    <Show
+      when={false}
+      fallback={
+        <EmptyProfilePageMessage
+          title={sample(NO_DATA_MESSAGES.title)}
+          description={sample(NO_DATA_MESSAGES.description)}
+        >
+          <Button size="lg">Post a Moment</Button>
+        </EmptyProfilePageMessage>
+      }
+    >
+      Moments feed
+    </Show>
+  );
 }
