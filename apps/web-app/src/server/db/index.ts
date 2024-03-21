f2692d810073450396e3147a3f168d1576f8b1e1
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-21 07:52:30 +0000

    He's finally done it

--- apps/web-app/src/server/db/index.ts
+++ apps/web-app/src/server/db/index.ts
@@ -1,8 +1,10 @@
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
+import * as schema from "~/server/db/schemas";
+
 import config from "~/server/db/config";
 
 const queryClient = postgres(config.dbCredentials.connectionString);
 
-export const db = drizzle(queryClient);
+export const db = drizzle(queryClient, { schema });
---
+++ apps/web-app/src/server/db/schemas/index.ts
@@ -0,0 +1,5 @@
+// TODO: Make util to gen this file automatically
+
+export * from "~/server/db/schemas/actors";
+export * from "~/server/db/schemas/invite-codes";
+export * from "~/server/db/schemas/records";
