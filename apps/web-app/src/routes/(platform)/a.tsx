8fa93a5f10d77ed4a56c5ae4d03faf11353281bf
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-26 10:40:17 +0000

    Key the ALayout. Fetch the Actor here and provide value to context

--- apps/web-app/src/routes/(platform)/a.tsx
+++ apps/web-app/src/routes/(platform)/a.tsx
@@ -1,7 +1,56 @@
-import { RouteSectionProps } from "@solidjs/router";
+import {
+  RouteSectionProps,
+  cache,
+  createAsync,
+  redirect,
+  useParams,
+} from "@solidjs/router";
+import { ParentProps, Show } from "solid-js";
 
 import { ActorRouteProvider } from "~/components/context/actor-route";
 
-export default function ALayout(props: RouteSectionProps) {
-  return <ActorRouteProvider>{props.children}</ActorRouteProvider>;
+import { findOneByPID$ } from "~/server/modules/actors/rpc";
+
+import { rpcSuccessResponse } from "~/lib/utils/rpc";
+
+import { paths } from "~/lib/constants/paths";
+
+const routeData = cache(async (actorPublicId: string) => {
+  "use server";
+
+  if (!actorPublicId) {
+    throw redirect(paths.notFound);
+  }
+
+  const matchingActor = await findOneByPID$(actorPublicId);
+
+  if (!matchingActor) {
+    throw redirect(paths.notFound);
+  }
+
+  return rpcSuccessResponse(matchingActor);
+}, "actor:layout");
+
+export type RouteDataType = typeof routeData;
+
+export default function ALayoutWrapper(props: RouteSectionProps) {
+  const params = useParams();
+
+  return (
+    <Show when={params.actorPublicId} keyed>
+      <ALayout>{props.children}</ALayout>
+    </Show>
+  );
+}
+
+function ALayout(props: ParentProps) {
+  const params = useParams();
+
+  const actor = createAsync(() => routeData(params.actorPublicId));
+
+  return (
+    <ActorRouteProvider actorAccessor={actor}>
+      {props.children}
+    </ActorRouteProvider>
+  );
 }
