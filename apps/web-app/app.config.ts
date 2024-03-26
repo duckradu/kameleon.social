c6b607b061e3c9e84e46e78ac2ba3a74cdd2143d
2blackholes2 <radu.dascalu97@gmail.com>
2024-03-26 07:58:07 +0000

    Install & setup solid-devtoold

--- apps/web-app/app.config.ts
+++ apps/web-app/app.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from "unocss/vite";
 import Icons from "unplugin-icons/vite";
 import { FileSystemIconLoader } from "unplugin-icons/loaders";
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
   vite: {
     plugins: [
+      devtools({
+        autoname: true,
+        locator: {
+          targetIDE: "vscode",
+          key: "Control",
+          jsxLocation: true,
+          componentLocation: true,
+        },
+      }),
       Icons({
         customCollections: {
           local: FileSystemIconLoader("./public/icons"),
